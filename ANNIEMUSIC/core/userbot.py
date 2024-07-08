e44a26f7836b54360b90c56d0937cf71288ce332
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-07-08 16:14:17 +0530

    Update userbot.py

--- ANNIEMUSIC/core/userbot.py
+++ ANNIEMUSIC/core/userbot.py
@@ -11,7 +11,7 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="Annie1",
+            name="Hot_me_pro",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
@@ -51,9 +51,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Dora_hub")
+                await self.one.join_chat("BROKENXNETWORK")
               
-                await self.one.join_chat("loggchk")
+                await self.one.join_chat("BROKNXSUPPORT")
             except:
                 pass
             assistants.append(1)
@@ -74,9 +74,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Dora_hub")
+                await self.two.join_chat("BROKENXNETWORK")
                 
-                await self.one.join_chat("loggchk")
+                await self.one.join_chat("BROKNXSUPPORT")
             except:
                 pass
             assistants.append(2)
@@ -96,9 +96,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Dora_hub")
+                await self.three.join_chat("BROKENXNETWORK")
                 
-                await self.one.join_chat("loggchk")
+                await self.one.join_chat("BROKNXSUPPORT")
             except:
                 pass
             assistants.append(3)
@@ -118,9 +118,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Dora_hub")
+                await self.four.join_chat("BROKENXNETWORK")
                 
-                await self.one.join_chat("loggchk")
+                await self.one.join_chat("BROKNXSUPPORT")
             except:
                 pass
             assistants.append(4)
@@ -140,9 +140,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Dora_hub")
+                await self.five.join_chat("BROKENXNETWORK")
                 
-                await self.one.join_chat("loggchk")
+                await self.one.join_chat("BROKNXSUPPORT")
             except:
                 pass
             assistants.append(5)
